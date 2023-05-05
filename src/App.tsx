5b1372eed349fd8aad524c710ebc53c3410bc0f4
ragnrayn <ragnrayn@gmail.com>
2023-05-05 21:28:57 +0300

    added ts-ignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home/Home'
+// @ts-ignore
 import AnimatedCursor from "react-animated-cursor"
 
 function App() {
