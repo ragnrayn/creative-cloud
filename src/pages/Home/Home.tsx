bb412bfec1bc4395c74d59ff91a94cf0ad67aeaa
ragnrayn <ragnrayn@gmail.com>
2023-05-06 19:09:40 +0300

    animation letters and numbers

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,40 +3,84 @@ import backgroundVideo from "../../assets/background_video.mp4";
 import backgroundAudio from "../../assets/tenet.mp3";
 import FollowModal from "../../components/FollowModal/FollowModal";
 import { useEffect, useState } from "react";
+import headerLogo from "../../assets/creative-cloud-logo.png";
 
 function Home() {
 
     const [isFollowOpen, setIsFollowOpen] = useState<boolean>(false);
     const [audio] = useState(new Audio(backgroundAudio));
-    const [toggleAudio, setToggleAudio] = useState(true);
+    const [toggleAudio, setToggleAudio] = useState(false);
+
+
+    const [textFirst, setTextFirst] = useState("SI");
+    const [textSecond, setTextSecond] = useState("JNJ");
+    const [textThird, setTextThird] = useState(100);
+    const [textFour, setTExtFour] = useState(355);
+    const [textFive, setTextFive] = useState("N");
+    const [textSix, setTextSix] = useState(22);
 
     useEffect(() => {
-        console.log(toggleAudio);
-        if(toggleAudio === true){
+        if (toggleAudio === true) {
             audio.play();
             return;
         }
         audio.pause();
     }, [toggleAudio])
 
+    const handleMove = () => {
+        let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+        let randomLetter = Math.floor(0 + Math.random() * alphabet.length);
+        let randomSecondLetter = Math.floor(0 + Math.random() * alphabet.length);
+        let randomThirdLetter = Math.floor(0 + Math.random() * alphabet.length);
+
+        let randomTo355 = Math.floor(0 + Math.random() * 355);
+        let randomTo100 = Math.floor(0 + Math.random() * 100);
+        let randomTo22 = Math.floor(0 + Math.random() * 22)
+
+        setTextFirst(alphabet[randomLetter].concat(alphabet[randomSecondLetter]));
+        setTextSecond(alphabet[randomLetter].concat(alphabet[randomSecondLetter]).concat(alphabet[randomThirdLetter]));
+        setTextThird(randomTo100);
+        setTExtFour(randomTo355);
+        setTextFive(alphabet[randomLetter]);
+        setTextSix(randomTo22);
+    }
+
     return (
         <>
-            <div className="home">
+            <div className="home" onMouseMove={handleMove} onClick={() => setToggleAudio(!toggleAudio)}>
                 <video id="background-video" loop autoPlay muted>
                     <source src={backgroundVideo} type="video/mp4" />
                 </video>
 
                 <div className="home-page">
-                    <div className="mute-btn">
-                        <button onClick={() => setToggleAudio(!toggleAudio)}><span className="circle"></span></button>
+                    <div className="home-page__header">
+                        <div className="header-logo">
+                            <a href="#">
+                                <img src={headerLogo} width={"50px"} alt="" />
+                            </a>
+                        </div>
+                        <div className="mute-btn">
+                            <button onClick={() => setToggleAudio(!toggleAudio)}><span className="circle"></span></button>
+                        </div>
                     </div>
                     <div className="soon">
                         comming soon
-                        <div className="contact" onClick={() => setIsFollowOpen(true)}>
+                        <div className="contact" onClick={(e) => { setIsFollowOpen(true); e.stopPropagation() }}>
                             <button style={{ transform: isFollowOpen ? "rotate(45deg)" : "" }}>
                                 <span className="circle"></span>
                             </button>
                             <div className="contact-text"><span className="contact-primary">contact</span> us</div>
+                        </div>
+                        <div className="coords">
+                            <div className="coords-text">
+                                <div className="text-first">{ textFirst }</div>
+                                <div className="text-second">{ textSecond }</div>
+                                <div className="text-third">{ textThird }</div>
+                                <div className="text-four">{ textFour }</div>
+                                <div className="text-five">{ textFive }</div>
+                                <div className="text-six">{ textSix }</div>
+                            </div>
+                            <div className="coords-line"></div>
                         </div>
                     </div>
                 </div>
